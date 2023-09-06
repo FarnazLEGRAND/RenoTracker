4f9520683de239683856a0e22c998773cb323110
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-09-06 11:11:13 +0200

    Initial Commit

---
+++ Renotracker/RenotrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenotrackerApp.swift
+//  Renotracker
+//
+//  Created by Farnaz Rismanchian on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RenotrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
