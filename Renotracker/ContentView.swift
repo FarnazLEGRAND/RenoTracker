9efc5539d379ed2d0089811350f76b5c785ff5f1
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-09-07 10:13:27 +0200

    customizing the appearance of view

--- Renotracker/ContentView.swift
+++ Renotracker/ContentView.swift
@@ -9,18 +9,83 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        VStack(alignment: .leading){
+            //            mark:header section
+            VStack(alignment: .leading){
+                Text("Frint Lobby")
+                    .font(.largeTitle)
+                
+//                Text("<<<Photo placehodore>>>")
+                Image("front-lobby")
+//                    .frame(width: 360, height: 300)
+                    .resizable()
+                    .scaledToFit()
+//                    .clipped()
+            }
+            // MARK: Status section
+                      VStack{
+                          Text("Due on Sunday, August 1, 2023")
+                          ProgressView(value: 0.6)
+                          Text("60% Complete")
+                          Text("Work Quality : Good")
+                          HStack{
+                              Image(systemName: "star.fill")
+                              Image(systemName: "star.fill")
+                              Image(systemName: "star.fill")
+                              Image(systemName: "star.fill")
+                              Image(systemName: "star")
+                          }.foregroundColor(.yellow)
+
+                          Text("FLAGGED FOR REVIEW")
+                      }
+                      
+                      Divider()
+                      
+                      // MARK: Punch List section
+                      VStack(alignment: .leading){
+                          Text("Punch List")
+                              .font(.headline)
+                              .foregroundColor(.accentColor)
+                          Label("Remodel front desk",systemImage: "checkmark.circle")
+                          Label("Retile entry",systemImage: "checkmark.circle")
+                          Label("Replace Light fixtures",systemImage: "checkmark.circle")
+                          Label("Paint walls", systemImage: "asterisk.circle")
+                          Label("Hang new artWork",systemImage: "circle")
+                      }
+                      // MARK: Budget section
+                      VStack(alignment: .leading){
+                          Text("Budget")
+                              .font(.headline)
+                              .foregroundColor(.accentColor)
+                          
+                          HStack{
+                              Text("Amount Allocated: ")
+                              Spacer()
+                              Text("$15,000")
+                          }
+                          HStack{
+                              Text("Spent to-date: ")
+                              Spacer()
+                              Text("$8,350")
+                          }
+                          HStack{
+                              Text("Amount remaining: ")
+                              Spacer()
+                              Text("$6,660")
+                          }
+                      }
+            Spacer()
+                  }
         .padding()
     }
 }
 
+
+//contene VIEUW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
