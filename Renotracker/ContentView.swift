b5c16a1b353ef1a03227b3b9f117c0c0253c82eb
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-09-07 11:13:31 +0200

    ajut subviews

--- Renotracker/ContentView.swift
+++ Renotracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             //            mark:header section
             Header()
             // MARK: Status section
-            ProgressInfoCart()
+            WorkQuality()
                       
                       Divider()
                       
@@ -58,18 +58,28 @@ struct Header: View {
                     RoundedRectangle(cornerRadius: 15)
                         .stroke(Color.white, lineWidth: 5)
                 )
+            // MARK: Progress section
+            ProgressInfoCard()
         }
     }
 }
 
-            
-// MARK: Progress section
-struct ProgressInfoCart: View {
+struct ProgressInfoCard: View {
     var body: some View {
         VStack{
+            HStack{
+                ProgressView(value: 0.6)
+                Text("60% Complete")
+            }
             Text("Due on Sunday, August 1, 2023")
-            ProgressView(value: 0.6)
-            Text("60% Complete")
+        }
+    }
+}
+
+// MARK: Status section
+struct WorkQuality: View {
+    var body: some View {
+        VStack{
             Text("Work Quality : Good")
             HStack{
                 Image(systemName: "star.fill")
@@ -78,11 +88,10 @@ struct ProgressInfoCart: View {
                 Image(systemName: "star.fill")
                 Image(systemName: "star")
             }.foregroundColor(.yellow)
-            
-            Text("FLAGGED FOR REVIEW")
         }
     }
 }
+
 // MARK: Punch List section
 struct PunchList: View {
     var body: some View {
@@ -90,14 +99,15 @@ struct PunchList: View {
             Text("Punch List")
                 .font(.headline)
                 .foregroundColor(.accentColor)
-            Label("Remodel front desk",systemImage: "checkmark.circle")
-            Label("Retile entry",systemImage: "checkmark.circle")
-            Label("Replace Light fixtures",systemImage: "checkmark.circle")
+            Label("Remodel front desk", systemImage: "checkmark.circle")
+            Label("Retile entry", systemImage: "checkmark.circle")
+            Label("Replace Light fixtures", systemImage: "checkmark.circle")
             Label("Paint walls", systemImage: "asterisk.circle")
-            Label("Hang new artWork",systemImage: "circle")
+            Label("Hang new artWork", systemImage: "circle")
         }
     }
 }
+
 // MARK: Budget section
 struct Budget: View {
     var body: some View {
@@ -106,12 +116,10 @@ struct Budget: View {
                 .font(.headline)
                 .foregroundColor(.accentColor)
             
-            Label(title:{
-                Text("On budget")
-            },icon: {
-                Image(systemName: "checkmark.circle.fill")
-                .foregroundColor(.green)})
-            
+            Label(
+                title: { Text("On Budget")},
+                icon: { Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.green)})
             
             HStack{
                 Text("Amount Allocated: ")
@@ -122,11 +130,14 @@ struct Budget: View {
                 Text("Spent to-date: ")
                 Spacer()
                 Text("$8,350")
+                    .underline()
             }
             HStack{
                 Text("Amount remaining: ")
+                    .bold()
                 Spacer()
                 Text("$6,660")
+                    .bold()
             }
         }
     }
